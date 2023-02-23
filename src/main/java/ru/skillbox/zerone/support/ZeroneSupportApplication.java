9906cbe28bb505ddc5524543d37d7277a85883b4
Vladimir Panfilov <vrpanfilov@yandex.ru>
2023-02-23 14:39:30 +0300

    Старт.

---
+++ src/main/java/ru/skillbox/zerone/support/ZeroneSupportApplication.java
@@ -0,0 +1,13 @@
+package ru.skillbox.zerone.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroneSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroneSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skillbox/zerone/support/ZeroneSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillbox.zerone.support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroneSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
